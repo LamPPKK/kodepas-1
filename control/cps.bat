88831b625f9ff39c3740a8585f459ebb4ea4a956
quocthinhvo <quocthinhvo0@gmail.com>
2020-06-25 11:39:23 +0700

    [Control] Thêm KodePas Beta Binary
    Có thể dùng song song KodePas cũ và trải nghiệm KodePas-Beta

--- control/cps.bat
+++ control/cps.bat
@@ -1,3 +1,3 @@
 del /S /Q /F M:\kode\kodegit\kodepas\control
 xcopy M:\kode\src\control M:\kode\kodegit\kodepas\control /h /i /c /k /e /r /y
-cp M:\kode\src\control\kodepas.exe M:\kode\kodegit\bin\kodepas-beta.exe
+xcopy M:\kode\src\control\kodepas.exe M:\kode\kodegit\kodepas\bin\kodepas-beta.exe
