828b6cb0a1ae015e01127df0764cd6418ecda79c
quocthinhvo <quocthinhvo0@gmail.com>
2020-06-25 11:54:47 +0700

    [Control] KPBuild thêm tính năng đọc INI
    Gồm đọc thử dữ liệu

--- control/build/kpbuild.pas
+++ control/build/kpbuild.pas
@@ -12,6 +12,7 @@ implementation
         
     end;
     procedure kpbuild_run;
+    var test: string;
     begin
         if (ParamStr(2) = '--help') then
             begin
@@ -28,5 +29,24 @@ implementation
                 kpprint_error('[Fatal] Build project stoped');
                 exit;
             end;
+        //Đọc dữ liệu từ local.ini
+        //Cấu trúc mẫu local.ini
+        //    [CONFIG] ;cái này là NODE quan trọng nhất
+        //    run=project.kode ;cái này là tệp khai báo dự án
+        //    type=(native/web/hibrid) ;cái này là loại dự án
+        //    bin=bin/project ;cái này là đường dẫn output cho Binary project, chỉ dành riêng cho Native và Hibrid
+        //    compiled=compiled/code ;cái này là đường dẫn output các tệp hậu biên dịch
+        //    sdk=kodepas //cái này khai báo SDK
+        //    [INFO] ;cái này thêm cho vui, chủ yếu là thông báo quản quyền
+        //    author=quocthinhvo ;cái này tên tác giả dự án
+        //    project=project ; cái này tên dự án khi phát hành lên Kode Store
+        //    ..=.. ;thêm không giới hạn và tùy theo loại và mục đích
+        writeln('[Start] Read data in local.ini');
+        //Đọc thử xem có lỗi không
+        test := kpini_string('local.ini', 'CONFIG', 'run');
+        //nếu đọc được thì thông báo không có lỗi
+        kpprint_complete('[Done ] Read data complete');
+        //Sau đó phân loại dự án nhằm chọn hướng xây dựng phù hợp theo từng loại
+        
     end;
 end.
