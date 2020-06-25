7d50391570c0e451aeced25a2e1bfe010261a092
quocthinhvo <quocthinhvo0@gmail.com>
2020-06-25 11:46:56 +0700

    [Control] KPBuild thêm kiểm tra project config

--- control/build/kpbuild.pas
+++ control/build/kpbuild.pas
@@ -13,6 +13,20 @@ implementation
     end;
     procedure kpbuild_run;
     begin
-
+        if (ParamStr(2) = '--help') then
+            begin
+                kpbuild_help;
+                exit;
+            end;
+        writeln('[Start] Build project');
+        writeln('[Check] Project cofig');
+        //Kiểm tra tệp local.ini còn không.
+        //Nếu còn thì đi tiếp còn mất thì báo lỗi đồng thời thoát khỏi kpbuild_run
+        if (not FileExists('local.ini')) then
+            begin
+                kpprint_error('[Error] Local.ini not found in project. Try "kodepas init --local" to create it');
+                kpprint_error('[Fatal] Build project stoped');
+                exit;
+            end;
     end;
 end.
