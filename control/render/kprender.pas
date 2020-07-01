6782dacf7e0d2b9ff0a49fd20519764e00e70f76
quocthinhvo <quocthinhvo0@gmail.com>
2020-07-01 13:07:29 +0700

    [Control] Hoàn thiện cơ bản KPRender #5

--- control/render/kprender.pas
+++ control/render/kprender.pas
@@ -1,6 +1,7 @@
 unit kprender;
 interface 
-    uses crt, sysutils, process;
+    uses crt, sysutils, process,
+        kpprint in './effect/print/kpprint.pas';
     procedure kprender_run;
     procedure kprender_help;
 implementation
@@ -12,11 +13,25 @@ implementation
         writeln('   [input list]: render resource from list');
     end;
     procedure kprender_run;
+    var cmdout: ansistring;
     begin
         if (paramStr(2) = '--help') then
             begin
                 kprender_help;
                 exit;
             end;
+        kpprint_process('[Start] Render');
+        if (runCommand('koderes ' + paramStr(2) + '.krs @' + paramStr(2), cmdout)) then
+            begin
+                kpprint_complete('[Done ] Render complete')
+            end
+        else
+            begin
+                kpprint_error('[Error] Render fail');
+                kpprint_error('[Code ]');
+                writeln(cmdout);
+                kpprint_error('[Fatal] Render stop');
+                exit;
+            end; 
     end;
 end.
