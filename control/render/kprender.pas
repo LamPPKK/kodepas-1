b9f1813f17561b6ba05a93254a05686211b1ca4d
quocthinhvo <quocthinhvo0@gmail.com>
2020-07-01 13:01:00 +0700

    [Control] Hướng dẫn dùng KPRender thông qua CLI #5

--- control/render/kprender.pas
+++ control/render/kprender.pas
@@ -6,10 +6,17 @@ interface
 implementation
     procedure kprender_help;
     begin
-        
+        writeln('Try: kodepas render [options] [value]');
+        writeln('Options:');
+        writeln('   --help: give help');
+        writeln('   [input list]: render resource from list');
     end;
     procedure kprender_run;
     begin
-
+        if (paramStr(2) = '--help') then
+            begin
+                kprender_help;
+                exit;
+            end;
     end;
 end.
