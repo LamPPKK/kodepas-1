66fb6a46468515a41914178b5561aac720020c7d
quocthinhvo <quocthinhvo0@gmail.com>
2020-07-03 09:44:32 +0700

    [Control] Kết nối cli_build với kpbuild #18 #20

--- control/build/kpbuild.pas
+++ control/build/kpbuild.pas
@@ -4,7 +4,8 @@ interface
         kpprint in './effect/print/kpprint.pas',
         kpini in './ini/kpini.pas',
         getos in './system/getos.pas',
-        native_build in 'native/native_build.pas';
+        native_build in 'native/native_build.pas',
+        cli_build in 'cli/cli_build.pas';
     procedure kpbuild_run;
     procedure kpbuild_help;
 implementation
@@ -52,6 +53,7 @@ implementation
         //Sau đó phân loại dự án nhằm chọn hướng xây dựng phù hợp theo từng loại
         case (kpini_string('local.ini', 'CONFIG','type')) of
             'native': native_build_run;
+            'cli': cli_build_run;
             else 
                 begin
                     kpprint_error('[Error] Unknow how to build (' + kpini_string('local.ini', 'CONFIG','type') +')');
