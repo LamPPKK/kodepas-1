b51da14d51b862cc473cfefb79598a40d6542950
quocthinhvo <quocthinhvo0@gmail.com>
2020-07-03 09:47:05 +0700

    [Control] Khởi tạo CLI_RUN (#18) (#20)

---
+++ control/run/cli/cli_run.pas
@@ -0,0 +1,24 @@
+unit cli_run;
+interface
+    uses crt, sysutils, process,
+        kpprint in '././effect/print/kpprint.pas',
+        kpini in '././ini/kpini.pas';
+    procedure cli_run_run;
+implementation
+    //lấy đường dẫn mẹ
+    function GetParent(input:string):string;
+        begin
+            exit(extractFileName(ExtractFileDir(input)));
+        end;
+    //lấy đường dẫn con
+    function GetChild(input:string):string;
+        begin
+            exit(extractFileName(input));
+        end;
+    procedure cli_run_run;
+    begin
+        kpprint_process('[Start] Run project');
+        ExecuteProcess(kpini_string('local.ini', 'CONFIG', 'bin'),'');
+        kpprint_complete('[Done ] Run complete');
+    end;
+end.
