c38979547fb9e0c25802c27c51e5ff56491050dc
quocthinhvo <quocthinhvo0@gmail.com>
2020-07-08 15:24:07 +0700

    [Control] Viết vài đoạn cơ bản cho NodeRun
    Thực tế là copy code từ Native Run vì bản chất nó khá giống nhau :v:

---
+++ control/run/node/node_run.pas
@@ -0,0 +1,24 @@
+unit node_run;
+interface
+    uses crt, sysutils, process,
+        kpprint in '././effect/print/kpprint.pas',
+        kpini in '././ini/kpini.pas';
+    procedure node_run_run;
+implementation
+    //lấy đường dẫn mẹ
+    function GetParent(input:string):string;
+        begin
+            exit(extractFileName(ExtractFileDir(input)));
+        end;
+    //lấy đường dẫn con
+    function GetChild(input:string):string;
+        begin
+            exit(extractFileName(input));
+        end;
+    procedure node_run_run;
+    begin
+        kpprint_process('[Start] Run project');
+        ExecuteProcess(kpini_string('local.ini', 'CONFIG', 'bin'),'');
+        kpprint_complete('[Done ] Run complete');
+    end;
+end.
